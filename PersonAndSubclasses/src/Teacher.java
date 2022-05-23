95c1c82cfb701cc2e7ceed45384402b1724abe03
kmiller <kmmiller418@gmail.com>
2022-05-23 13:12:23 -0400

    Modify files

---
+++ PersonAndSubclasses/src/Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher extends Person{
+    private int salary;
+
+    public Teacher(String name, String address, int salary){
+        super(name, address);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\n\t" + this.showSalary();
+    }
+
+    public String showSalary(){
+        return "salary " + salary + " euro/month";
+    }
+}
